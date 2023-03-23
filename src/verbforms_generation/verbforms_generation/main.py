3102c53415f36125166dafd2255e20381c61e745
Salome Wildermuth <salome.wildermuth@gmail.com>
2023-03-23 13:39:00 +0100

    flask experiment

--- src/verbforms_generation/verbforms_generation/main.py
+++ src/verbforms_generation/verbforms_generation/main.py
@@ -11,49 +11,16 @@ University of Zurich
 Institute for Computational Linguistics
 
 - main module for maintaining interaction with user
+- from https://flask.palletsprojects.com/en/1.1.x/quickstart/
+- start with flask run and open http://127.0.0.1:5000/ in the browser
 
 """
-from .termination_criterion import TerminationCriterionForConversation
-import time
-import warnings
-
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-
-    """
-    from src.conversation.conversation.termination_criterion import TerminationCriterionForConversation
-    from src.conversation_turn.conversation_turn.turn import ConversationTurn
-    from src.conversation.conversation.conversation import Conversation, Language
-    """
 
 
-def main():
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        cli = CLI()
-        cli.maintain_interaction()
 
+from flask import Flask
+app = Flask(__name__)
 
-class CLI:
-    def __init__(self):
-        self.termination_criterion = TerminationCriterionForConversation()
-
-
-    def maintain_interaction(self):
-        turn_number = 1
-        print("Please enter a verb in any form, time or mode.")
-        while not self.termination_criterion.given():
-            verb = input()
-            verb = self.__validate(verb)
-            print("Please enter the nect verb in any form, time or mode.")
-
-
-    def __validate(self, user_input):
-        while not user_input:
-            user_input = input()
-        self.termination_criterion.check_user_terminate(user_input)
-        return user_input
-
-
-if __name__ == "__main__":
-    main()
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
