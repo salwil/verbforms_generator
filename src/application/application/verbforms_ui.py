33744e126ecfafb0a1b62911919104bfe185c41f
Salome Wildermuth <salome.wildermuth@gmail.com>
2023-04-23 17:07:04 +0200

    dont automatically add conjugation table to file, let user decide

--- src/application/application/verbforms_ui.py
+++ src/application/application/verbforms_ui.py
@@ -28,19 +28,43 @@ app = Flask(__name__)
 
 csv_file_for_verbs = CsvFile()
 
+class CurrentScreen():
+    def __init__(self):
+        self.verb = None
+
+current_screen = CurrentScreen()
+
 @app.route('/verbforms_generator')
 def welcome():
     return render_template('welcome.html')
 
 @app.route('/verbforms_generator', methods=['POST'])
 def verbforms_generator():
-    verb=request.form['verb']
-    verbforms = Verbforms(verb)
-    print(verbforms.verb.german_conjugations)
-    csv_file_for_verbs.write_record(verbforms.verb)
-    return render_template('verbforms_generator.html', verb=verbforms.verb.infinitive_german, conjugation_table=verbforms.verb.german_conjugations)
+    verb=request.form['next_verb']
+    current_screen.verb = Verbforms(verb)
+    print(current_screen.verb.verb.german_conjugations)
+    #csv_file_for_verbs.write_record(verbforms.verb)
+    return render_template('verbforms_generator.html',
+                           next_verb=current_screen.verb.verb.infinitive_german,
+                           english_translation=current_screen.verb.verb.infinitive_english,
+                           conjugation_table=current_screen.verb.verb.german_conjugations)
+
+@app.route('/verbforms_generator/add', methods=['POST'])
+def add_verb_to_list():
+    print(current_screen.verb.verb.german_conjugations)
+    csv_file_for_verbs.write_record(current_screen.verb.verb)
+    return render_template('verbforms_generator.html',
+                           next_verb=current_screen.verb.verb.infinitive_german,
+                           english_translation=current_screen.verb.verb.infinitive_english,
+                           conjugation_table=current_screen.verb.verb.german_conjugations)
 
 @app.route('/verbforms_generator/download')
 def download_file_with_verbs():
     csv_file_for_verbs.close_file()
-    return send_file(csv_file_for_verbs.file_path), render_template('welcome.html')
+    return send_file(csv_file_for_verbs.file_path)
+
+@app.route('/verbforms_generator/goodbye')
+def goodbye():
+    # todo :this endpoint should be called when the user hits "download" and after that they click another button to finally download the file
+    # this is to reduce erroronous behavior of the tool.
+    return render_template('goodbye.html')
