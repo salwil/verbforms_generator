6cb10911959429ce38312b7b2ffc3260c19c1cf5
saphjra <49561526+saphjra@users.noreply.github.com>
2023-05-14 20:16:15 +0200

    pototyp for automatically reading a csv file with two coll to generate anki cards throught api

--- proto_csv_to_anki/csv_to_anki_api.py
+++ proto_csv_to_anki/csv_to_anki_api.py
@@ -86,102 +86,8 @@ def execution_addNotes(csvfile, deckName="verbforms", action="addNotes", version
 
 
 
-
 if __name__ == "__main__":
-
-    notestest = {
-        "action": "canAddNotes",
-        "version": 6,
-        "params": {
-            "notes": [
-                {
-                    "deckName": "Default",
-                    "modelName": "Basic",
-                    "fields": {
-                        "Front": "hopend",
-                        "Back": "hopelessed"
-                    }
-                },
-                {
-                    "deckName": "verbgentest1",
-                    "modelName": "Basic",
-                    "fields": {
-                        "Front": "loved",
-                        "Back": "life"
-                    }
-                }]
-        }
-    }
-
-    guitest = {
-        "action": "guiAddCards",
-        "version": 6,
-        "params": {
-            "note": {
-                "deckName": "Default",
-                "modelName": "Cloze",
-                "fields": {
-                    "Text": "hope",
-                    "Extra": "Romania is a country in Europe"
-                },
-                "tags": [
-                    "countries"
-                ],
-                "picture": [{
-                    "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/EU-Romania.svg/285px-EU-Romania.svg.png",
-                    "filename": "romania.png",
-                    "fields": [
-                        "Extra"
-                    ]
-                }]
-            }
-        }
-    }
-
-    notetest_Web = {
-    "action": "addNotes",
-    "version": 6,
-    "params": {
-        "notes": [
-            {
-                "deckName": "test2",
-                "modelName": "Basic",
-                "fields": {
-                    "Front": "loooooo",
-                    "Back": "fuuuu"
-                },
-                "tags": [
-                    "yomichan"
-                ],
-                "audio": [{
-                    "url": "https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?kanji=猫&kana=ねこ",
-                    "filename": "yomichan_ねこ_猫.mp3",
-                    "skipHash": "7e2c2f954ef6051373ba916f000168dc",
-                    "fields": [
-                        "Front"
-                    ]
-                }],
-                "video": [{
-                    "url": "https://cdn.videvo.net/videvo_files/video/free/2015-06/small_watermarked/Contador_Glam_preview.mp4",
-                    "filename": "countdown.mp4",
-                    "skipHash": "4117e8aab0d37534d9c8eac362388bbe",
-                    "fields": [
-                        "Back"
-                    ]
-                }],
-                "picture": [{
-                    "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/A_black_cat_named_Tilly.jpg/220px-A_black_cat_named_Tilly.jpg",
-                    "filename": "black_cat.jpg",
-                    "skipHash": "8d6e4646dfae812bf39651b59d7429ce",
-                    "fields": [
-                        "Back"
-                    ]
-                }]
-            }
-        ]
-    }
-}
-
+    print(invoke("modelNames"))
 
     #invoke('createDeck', deck='test2')
     #print(invokejson(notetest_Web))
@@ -194,4 +100,4 @@ if __name__ == "__main__":
     #new_notes = param_creator()
     #invokejson(notetest_Web)
 
-    execution_addNotes("test.tsv")
+    execution_addNotes("test.tsv", modelName='verbformmodel ')
