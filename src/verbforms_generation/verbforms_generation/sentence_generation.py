f205a0310041fa6f46a5e0cf8f6d83c52bc9201a
Salome Wildermuth <salome.wildermuth@gmail.com>
2023-05-14 13:25:36 +0200

    remove English sentence generation (no longer used)

--- src/verbforms_generation/verbforms_generation/sentence_generation.py
+++ src/verbforms_generation/verbforms_generation/sentence_generation.py
@@ -16,7 +16,7 @@ Institute for Computational Linguistics
 import random
 import re
 
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModelWithLMHead, pipeline
+from transformers import AutoTokenizer, AutoModelWithLMHead, pipeline
 
 
 class SentenceGenerator:
@@ -26,13 +26,6 @@ class SentenceGenerator:
         self.model = AutoModelWithLMHead.from_pretrained(checkpoint)
         self.valid_sentence = False
         # English sentence generation:
-        '''
-        checkpoint = "gpt2-large"
-        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-        self.model = AutoModelForCausalLM.from_pretrained(checkpoint)
-        self.pronouns = ['I', 'I', 'You', 'You', 'He', 'She', 'She', 'It', 'We', 'We', 'You', 'They', 'They']
-        self.verb_be = {'I': 'am', 'You':'are', 'He': 'is', 'She': 'is', 'It': 'is', 'We':'are', 'They':'are'}
-        '''
 
     def generate(self, conjugation_table: list, nr_of_tokens: int = 15):
         while (not self.valid_sentence):
@@ -52,27 +45,3 @@ class SentenceGenerator:
             sentence_as_list[0] = sentence_as_list[0].capitalize()
         return ' '.join(sentence_as_list) + '.'
 
-        # English sentence generation:
-        '''
-        pronoun = random.choice(self.pronouns)
-        input = input.replace('to ', '')
-        if input == 'be':
-            verb_clause = pronoun + ' ' + self.verb_be[pronoun]
-        else:
-            if pronoun == 'He' or pronoun == 'She' or pronoun == 'It':
-                if input.endswith(('a','i','o','u')):
-                    verb_clause = pronoun + ' ' + input + 'es'
-                elif input.endswith('y'):
-                    verb_clause = input.rstrip('y')
-                    verb_clause = pronoun + ' ' + verb_clause + 'ies'
-                else:
-                    verb_clause = pronoun + ' ' + input + 's'
-            else:
-                verb_clause = pronoun + ' ' + input
-        inputs = self.tokenizer(verb_clause, return_tensors="pt")
-        #elapsed time with 12 max_tokens: 0:01:14.457686, 20 max_tokens: 0:02:01.105493
-        outputs = self.model.generate(**inputs, penalty_alpha=0.6, top_k=4, max_new_tokens=nr_of_tokens)
-        out = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
-        pattern = re.compile(r"[;.!:]")
-        out = pattern.split(out)
-        return out[0] + '.'''''
--- test/verbforms_generation/sentence_generation_test.py
+++ test/verbforms_generation/sentence_generation_test.py
@@ -5,101 +5,55 @@ from src.verbforms_generation.verbforms_generation.sentence_generation import Se
 
 generator = SentenceGenerator()
 
+
 class TranslationTest(unittest.TestCase):
 
     def test_generate_sentence_for_gehen(self):
         print(generator.generate([('ich geh(e)',
-              'ich ging',
-              'ich bin gegangen',
-              'ich war gegangen',
-              'ich werde gegangen sein',
-              'ich werde geh(e)n',
-              'ich gehe',
-              'ich ginge'),
-             ('du gehst',
-              'du gingst',
-              'du bist gegangen',
-              'du warst gegangen',
-              'du wirst gegangen sein',
-              'du wirst geh(e)n',
-              'du gehest',
-              'du gingest'),
-             ('er geht',
-              'er ging',
-              'er ist gegangen',
-              'er war gegangen',
-              'er wird gegangen sein',
-              'er wird geh(e)n',
-              'er gehe',
-              'er ginge'),
-             ('wir geh(e)n',
-              'wir gingen',
-              'wir sind gegangen',
-              'wir waren gegangen',
-              'wir werden gegangen sein',
-              'wir werden geh(e)n',
-              'wir geh(e)n',
-              'wir gingen'),
-             ('ihr geht',
-              'ihr gingt',
-              'ihr seid gegangen',
-              'ihr wart gegangen',
-              'ihr werdet gegangen sein',
-              'ihr werdet geh(e)n',
-              'ihr gehet',
-              'ihr ginget'),
-             ('sie geh(e)n',
-              'sie gingen',
-              'sie sind gegangen',
-              'sie waren gegangen',
-              'sie werden gegangen sein',
-              'sie werden geh(e)n ',
-              'sie geh(e)n ',
-              'sie gingen ')]))
-
-        # this was for English sentence generation:
-        '''
-        def test_generate_sentence_for_try(self):
-            d1 = datetime.now()
-            print(generator.generate('try'))
-            d2 = datetime.now()
-            print('Elapsed time:')
-            print(d2 - d1)
-            d1 = datetime.now()
-            print(generator.generate('try', 20))
-            d2 = datetime.now()
-            print('Elapsed time:')
-            print(d2 - d1)
-
-        def test_generate_sentence_for_go(self):
-            print(generator.generate('gehen'))
-            print(generator.generate('go', 20))
-
-        def test_generate_sentence_for_be(self):
-            print(generator.generate('be', 20))
-            print(generator.generate('be'))
-            print(generator.generate('be'))
-            print(generator.generate('be'))
-
-        def test_generate_sentence_for_walk(self):
-            print(generator.generate('walk', 20))
-
-        def test_generate_sentence_for_speak(self):
-            print(generator.generate('speak', 20))
-
-        def test_generate_sentence_for_walk(self):
-            print(generator.generate('put'))
-
-        def test_generate_sentence_for_feel(self):
-            print(generator.generate('feel'))
-
-        def test_generate_sentence_for_stand(self):
-            d1 = datetime.now()
-            print(d1)
-            print(generator.generate('stand'))
-            d2 = datetime.now()
-            print(d2)
-            print('Elapsed time:')
-            print(d2 - d1)
-        '''
-
+                                   'ich ging',
+                                   'ich bin gegangen',
+                                   'ich war gegangen',
+                                   'ich werde gegangen sein',
+                                   'ich werde geh(e)n',
+                                   'ich gehe',
+                                   'ich ginge'),
+                                  ('du gehst',
+                                   'du gingst',
+                                   'du bist gegangen',
+                                   'du warst gegangen',
+                                   'du wirst gegangen sein',
+                                   'du wirst geh(e)n',
+                                   'du gehest',
+                                   'du gingest'),
+                                  ('er geht',
+                                   'er ging',
+                                   'er ist gegangen',
+                                   'er war gegangen',
+                                   'er wird gegangen sein',
+                                   'er wird geh(e)n',
+                                   'er gehe',
+                                   'er ginge'),
+                                  ('wir geh(e)n',
+                                   'wir gingen',
+                                   'wir sind gegangen',
+                                   'wir waren gegangen',
+                                   'wir werden gegangen sein',
+                                   'wir werden geh(e)n',
+                                   'wir geh(e)n',
+                                   'wir gingen'),
+                                  ('ihr geht',
+                                   'ihr gingt',
+                                   'ihr seid gegangen',
+                                   'ihr wart gegangen',
+                                   'ihr werdet gegangen sein',
+                                   'ihr werdet geh(e)n',
+                                   'ihr gehet',
+                                   'ihr ginget'),
+                                  ('sie geh(e)n',
+                                   'sie gingen',
+                                   'sie sind gegangen',
+                                   'sie waren gegangen',
+                                   'sie werden gegangen sein',
+                                   'sie werden geh(e)n ',
+                                   'sie geh(e)n ',
+                                   'sie gingen ')]))
