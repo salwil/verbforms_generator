62613678bf0d1df1cbeb32790c4833b5625c5cf8
Salome Wildermuth <salome.wildermuth@gmail.com>
2023-05-14 20:17:15 +0200

    create deck and should create cards, but does not do the latter yet

--- test/verbforms_generation/csv_to_anki_test.py
+++ test/verbforms_generation/csv_to_anki_test.py
@@ -6,7 +6,5 @@ from src.verbforms_generation.verbforms_generation.csv_to_anki import CSVExport
 class CSVExportTest(unittest.TestCase):
 
     def test_base_case(self):
-        csv_export = CSVExport('14-05-2023-13-28-52-index-cards.csv')
-        csv_export.invoke('createDeck', deck='test1')
-        result = csv_export.invoke('deckNames')
-        print('got list of decks: {}'.format(result))
+        csv_export = CSVExport('../../index_cards/14-05-2023-13-28-52-index-cards.csv')
+        csv_export.add_notes()
