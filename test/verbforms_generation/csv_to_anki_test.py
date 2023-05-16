a9b0f4d90fd0eaa50ab7b2c8f4d6f9dff0e80b68
Salome Wildermuth <salome.wildermuth@gmail.com>
2023-05-16 08:07:17 +0200

    connect to anki and insert deck and cards

--- test/verbforms_generation/csv_to_anki_test.py
+++ test/verbforms_generation/csv_to_anki_test.py
@@ -1,10 +1,36 @@
 import unittest
 
 from src.verbforms_generation.verbforms_generation.csv_to_anki import CSVExport
-
+import json
+import urllib.request
 
 class CSVExportTest(unittest.TestCase):
 
     def test_base_case(self):
-        csv_export = CSVExport('../../index_cards/14-05-2023-13-28-52-index-cards.csv')
+        csv_export = CSVExport('../../index_cards/14-05-2023-13-28-51-index-cards.csv', model_name='Basic')
         csv_export.add_notes()
+
+    def test_can_add_notes(self):
+        request_obj = {
+            "action": "canAddNotes",
+            "version": 6,
+            "params": {
+                "notes": [
+                    {
+                        "deckName": "Default",
+                        "modelName": "Basic",
+                        "fields": {
+                            "Front": "front content",
+                            "Back": "back content"
+                        },
+                        "tags": [
+                            "yomichan"
+                        ]
+                    }
+                ]
+            }
+        }
+
+        request_jsonob = json.dumps(request_obj).encode('utf-8')
+        response = json.load(urllib.request.urlopen(urllib.request.Request('http://localhost:8765', request_jsonob)))
+        print(response)
